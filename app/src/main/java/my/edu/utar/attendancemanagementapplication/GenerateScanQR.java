98a7f6541b3bd80f76a86de5d4432e7d917d57d9
Lewtp <108920525+Lewtp@users.noreply.github.com>
2023-04-09 21:54:17 +0800

    updated

---
+++ app/src/main/java/my/edu/utar/attendancemanagementapplication/GenerateScanQR.java
@@ -0,0 +1,14 @@
+package my.edu.utar.attendancemanagementapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class GenerateScanQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate_scan_qr);
+    }
+}
