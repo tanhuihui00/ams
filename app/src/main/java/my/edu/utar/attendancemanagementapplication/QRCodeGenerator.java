612cc79a1ad7fe904e5143196d0401915ba5bf3a
Lewtp <108920525+Lewtp@users.noreply.github.com>
2023-04-16 19:04:32 +0800

    .

--- app/src/main/java/my/edu/utar/attendancemanagementapplication/QRCodeGenerator.java
+++ app/src/main/java/my/edu/utar/attendancemanagementapplication/QRCodeGenerator.java
@@ -3,80 +3,129 @@ package my.edu.utar.attendancemanagementapplication;
 import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class QRCodeGenerator extends AppCompatActivity {
-    private EditText sessionNameEditText;
-    private EditText sessionDateEditText;
-    private EditText sessionTimeEditText;
-    private Button createSessionButton;
+
     private ImageView qrCodeImageView;
+    private Spinner attendanceIdSpinner;
+    private Button generateQrCodeButton;
+   
+
+    private static final String SUPABASE_URL = "https://wezvcdkmgwkuwlmmkklu.supabase.co/rest/v1/Attendances?select=attendanceID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode_generator);
-        sessionNameEditText = findViewById(R.id.session_name_edittext);
-        sessionDateEditText = findViewById(R.id.session_date_edittext);
-        sessionTimeEditText = findViewById(R.id.session_time_edittext);
-        createSessionButton = findViewById(R.id.create_session_button);
-        qrCodeImageView = findViewById(R.id.qr_code_imageview);
 
+        qrCodeImageView = findViewById(R.id.qr_code_image_view);
+        attendanceIdSpinner = findViewById(R.id.attendance_id_spinner);
+        generateQrCodeButton = findViewById(R.id.generate_qr_code_button);
 
-        createSessionButton.setOnClickListener(new View.OnClickListener() {
+        //retrieve the attendanceID from the database
+        new RetrieveAttendanceIdsTask().execute();
+
+        generateQrCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = sessionNameEditText.getText().toString();
-                String date = sessionDateEditText.getText().toString();
-                String time = sessionTimeEditText.getText().toString();
+                String attendanceId = attendanceIdSpinner.getSelectedItem().toString();
 
                 // generate the QR code
-                Bitmap qrCode = generateQrCode(name, date, time);
-
-                // create a new Session object
-                Class_Session session = new Class_Session(name, date, time, qrCode);
-
-                // TODO: store the session in the database
-
-                // show an alert dialog to indicate that the session was created successfully
-                AlertDialog.Builder builder = new AlertDialog.Builder(QRCodeGenerator.this);
-                builder.setTitle("Success")
-                        .setMessage("Class session created successfully.")
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // TODO: handle OK button click
-                            }
-                        })
-                        .show();
+                Bitmap qrCode = generateQrCode(attendanceId);
 
                 // display the QR code in the ImageView
                 qrCodeImageView.setImageBitmap(qrCode);
+
+                // show success dialog
+                showSuccessDialog(attendanceId);
             }
         });
+
+        // add an onItemSelectedListener to the Spinner
+        attendanceIdSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String attendanceId = parent.getItemAtPosition(position).toString();
+                Toast.makeText(QRCodeGenerator.this, "Selected attendance ID: " + attendanceId, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
     }
 
-    private Bitmap generateQrCode(String name, String date, String time) {
-        // concatenate the session details into a single string
-        //String sessionDetails = name + ", " + date + ", " + time;
-        String sessionId = "123456";
+    private class RetrieveAttendanceIdsTask extends AsyncTask<Void, Void, List<String>> {
+        @Override
+        protected List<String> doInBackground(Void...voids) {
+            List<String> attendanceIds = new ArrayList<>();
+
+            try{
+                URL url = new URL(SUPABASE_URL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("apikey", getString(R.string.apikey));
+                connection.setRequestProperty("Authorization", "Bearer " + getString(R.string.apikey));
+
+                if (connection.getResponseCode() == HttpURLConnection.HTTP_OK){
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    String line = null;
+
+                    while ((line = reader.readLine())!= null) {
+                        // Add the entire response string to the attendanceIds list
+                        attendanceIds.add(line);
+                    }
+                    reader.close();
+                }
+                connection.disconnect();
+        } catch(IOException e){
+                e.printStackTrace();
+            }
+
+            return attendanceIds;
+    }
+
+        @Override
+        protected void onPostExecute(List<String> attendanceIds) {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(QRCodeGenerator.this, android.R.layout.simple_spinner_item, attendanceIds);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            attendanceIdSpinner.setAdapter(adapter);
+        }
+    }
+
+    private Bitmap generateQrCode(String attendanceId) {
         // create a QR code writer
         QRCodeWriter writer = new QRCodeWriter();
 
         try {
-            // encode the session details as a bit matrix
-            BitMatrix bitMatrix = writer.encode(sessionId, BarcodeFormat.QR_CODE, 512, 512);
+            // encode the attendance ID as a bit matrix
+            BitMatrix bitMatrix = writer.encode(attendanceId, BarcodeFormat.QR_CODE, 512, 512);
 
             // convert the bit matrix to a bitmap
             int width = bitMatrix.getWidth();
@@ -94,5 +143,23 @@ public class QRCodeGenerator extends AppCompatActivity {
             return null;
         }
     }
+
+    private void showSuccessDialog(String attendanceId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("QR code for attendance ID " + attendanceId + " has been generated successfully!")
+                .setTitle("QR code generated")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // dismiss the dialog
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
+
+
 
